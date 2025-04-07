13797bf998738e9aad48228f422d34ab5f61bdda
vishnuTestEng <vilvavishnu13@gmail.com>
2025-04-07 15:22:34 +0530

    2nd time practising

---
+++ src/test/java/com/test/Oops.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Oops {
+	public void empName() {
+		System.out.println("Employee name is: Akilan");
+		
+	}
+	public void empGender() {
+		System.out.println("Employee gender is:Male");
+		
+	}
+
+}
